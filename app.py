5fafb0c2e0ede883c1a03c7e5701573d37a32799
pcarneiro07 <pedroliveiragc2000@yahoo.com>
2025-11-21 19:43:04 +0000

    fix: bind Dash to 0.0.0.0

--- app.py
+++ app.py
@@ -209,4 +209,9 @@ def atualizar(produto, aba):
 
 # ================================================================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=8050,
+        debug=False
+    )
+
